d9c713d05a7c306c2c383566967218e9e8dcfd9f
leximainw <leximainw@gmail.com>
2022-10-13 19:44:42 -0400

    allow JS to handle overskilled languages

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 [...document.querySelectorAll('#languages .skill')]
     .forEach(elem => {
-        elem.style.width = `${elem.innerText}%`
+        if (elem.innerText > 100) {
+            const overskillDiv = document.createElement('div')
+            overskillDiv.classList.add('overskilled')
+            const overskillImg = document.createElement('img')
+            overskillImg.src = 'chevron-double-right.svg'
+            overskillImg.alt = ''
+            overskillDiv.append(overskillImg)
+            elem.parentElement.after(overskillDiv)
+        }
+        elem.style.width = `${Math.min(elem.innerText, 100)}%`
         elem.innerText = ''
     })
